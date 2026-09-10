3d1ccc615cd4b47284df9a3f7799741154d6e6bc
Olivier Petrucciani <olivier+dev-server@petrucciani.net>
2026-09-10 09:39:49 +0200

    erreur de frapp  rsync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -76,7 +76,7 @@ rsync -avz --delete $DRY_RUN \
     --exclude='users/' \
     --exclude='public/photos/' \
     --exclude='public/build/' \
-    --exlude='content/'
+    --exclude='content/'
     ./ $REMOTE:$REMOTE_PATH/
 
 # Assets compilés : sync sans --delete (ne jamais supprimer un upload)
